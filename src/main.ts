dad3f96358b640508075cdfd218b06a53331e49d
Scott Gebert <scott22.gebert@gmail.com>
2021-10-22 16:22:50 -0600

    Setting up docs folder

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app/app.component';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
